c3936b2b3a83ca863eca2bf4a81461c38c4bbfa9
aleksandrfoman <50654588+aleksandrfoman@users.noreply.github.com>
2022-05-15 13:19:25 +0300

    AddNewSounds+NewJump+startPool

--- 3D_Platformer/Assets/Scripts/Player.cs
+++ 3D_Platformer/Assets/Scripts/Player.cs
@@ -14,6 +14,8 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float groundCheckRadius;
     [SerializeField]
+    private bool isJump;
+    [SerializeField]
     Transform groundCheck;
     [SerializeField]
     private new Rigidbody rigidbody;
@@ -21,6 +23,8 @@ public class Player : MonoBehaviour
     private new Collider collider;
     [SerializeField]
     private GameObject playerJumpEffect;
+    [SerializeField]
+    private float checkHitDist;
     RaycastHit hit;
 
     public UnityEvent OnJump;
@@ -35,6 +39,7 @@ public class Player : MonoBehaviour
     {
         MoveLogic();
         JumpLogic();
+        CheckSpawn();
     }
     private Vector3 movementVector
     {
@@ -54,32 +59,38 @@ public class Player : MonoBehaviour
         transform.Translate(movementVector * moveSpeed * Time.fixedDeltaTime);
     }
 
-    private void JumpLogic()
+    private void CheckSpawn()
     {
-        if (isGrounded && (Input.GetAxis("Jump") > 0))
+        //Physics.SphereCast(transform.position, checkHitRadius, Vector3.down, out hit, checkHitDist, groundLayer);
+        Physics.Raycast(transform.position, Vector3.down, out hit, checkHitDist,groundLayer);
+        if (hit.collider != null)
         {
-            Instantiate(playerJumpEffect,groundCheck.position, Quaternion.identity);
-            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            OnJump.Invoke();
+            if (GameController.Instance.NextPlatform == null || hit.transform == GameController.Instance.CurrentPlatform.transform)
+            {
+                GameController.Instance.SpawnPlatform();
+            }
         }
     }
-
+    private void JumpLogic()
+    {
+        if(isGrounded && !isJump && (Input.GetAxis("Jump") > 0))
+        {
+            Instantiate(playerJumpEffect, groundCheck.position, Quaternion.identity);
+            OnJump.Invoke();
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isJump = true;
+        }
+        else if (isGrounded && isJump)
+        {
+            isJump = false;
+        }
+    }
     private bool isGrounded
     {
         get
         {
-            bool check = Physics.SphereCast(transform.position, groundCheckRadius, Vector3.down, out hit,3f, groundLayer);
-
-
-            if (hit.collider != null)
-            {
-                
-                if (GameController.Instance.NextPlatform == null || hit.transform == GameController.Instance.CurrentPlatform.transform)
-                {
-                    GameController.Instance.SpawnPlatform();
-                }
-            }
-
+            var bottomCenterPoint = new Vector3(collider.bounds.center.x, collider.bounds.min.y, collider.bounds.center.z);
+            bool check = Physics.CheckCapsule(collider.bounds.center, bottomCenterPoint, collider.bounds.size.x / 2 * groundCheckRadius, groundLayer);
             return check;
         }
     }
